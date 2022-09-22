c77d0924e6e6ae58fc889fd7ab4bf96337457684
Dmytro Svirsa <104418771+DmytroSvirsa@users.noreply.github.com>
2022-09-22 12:04:44 +0300

    tests form function added

--- tests/test_taxi-service-user-form-and-validation.py
+++ tests/test_taxi-service-user-form-and-validation.py
@@ -6,40 +6,26 @@ from taxi.forms import DriverLicenseUpdateForm
 
 
 class ValidLicenseNumberFormTests(TestCase):
-    def test_validation_license_number_with_valid_data(self):
-        test_license_number = "TES12345"
-        form = DriverLicenseUpdateForm(
+    @staticmethod
+    def create_form(test_license_number):
+        return DriverLicenseUpdateForm(
             data={"license_number": test_license_number}
         )
-        self.assertTrue(form.is_valid())
+
+    def test_validation_license_number_with_valid_data(self):
+        self.assertTrue(self.create_form("TES12345").is_valid())
 
     def test_length_of_license_number_should_be_not_more_than_8(self):
-        test_license_number = "TES123456"
-        form = DriverLicenseUpdateForm(
-            data={"license_number": test_license_number}
-        )
-        self.assertFalse(form.is_valid())
+        self.assertFalse(self.create_form("TES123456").is_valid())
 
     def test_length_of_license_number_should_be_not_less_than_8(self):
-        test_license_number = "TES1234"
-        form = DriverLicenseUpdateForm(
-            data={"license_number": test_license_number}
-        )
-        self.assertFalse(form.is_valid())
+        self.assertFalse(self.create_form("TES1234").is_valid())
 
     def test_first_3_characters_should_be_uppercase_letters(self):
-        test_license_number = "TE123456"
-        form = DriverLicenseUpdateForm(
-            data={"license_number": test_license_number}
-        )
-        self.assertFalse(form.is_valid())
+        self.assertFalse(self.create_form("TE123456").is_valid())
 
     def test_last_5_characters_should_be_be_digits(self):
-        test_license_number = "TEST23456"
-        form = DriverLicenseUpdateForm(
-            data={"license_number": test_license_number}
-        )
-        self.assertFalse(form.is_valid())
+        self.assertFalse(self.create_form("TEST2345").is_valid())
 
 
 class DriverViewsTest(TestCase):
