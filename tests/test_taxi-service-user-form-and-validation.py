d72a88fa9026c497ac266cdde32e3655c86c3ffa
danylott <d.tuytuyshkin@gmail.com>
2022-10-11 14:48:53 +0300

    Added flake8 plugins and fixes for it

--- tests/test_taxi-service-user-form-and-validation.py
+++ tests/test_taxi-service-user-form-and-validation.py
@@ -35,29 +35,23 @@ class DriverViewsTest(TestCase):
             license_number="ADM12345",
             first_name="Admin",
             last_name="User",
-            password="1qazcde3"
+            password="1qazcde3",
         )
         self.client.force_login(self.user)
 
     def test_update_driver_license_number_with_valid_data(self):
         test_license_number = "ADM22345"
         response = self.client.post(
-            reverse(
-                "taxi:driver-update",
-                kwargs={"pk": self.user.id}
-            ),
-            data={"license_number": test_license_number}
+            reverse("taxi:driver-update", kwargs={"pk": self.user.id}),
+            data={"license_number": test_license_number},
         )
         self.assertEqual(response.status_code, 302)
 
     def test_update_driver_license_number_with_not_valid_data(self):
         test_license_number = "a5"
         response = self.client.post(
-            reverse(
-                "taxi:driver-update",
-                kwargs={"pk": self.user.id}
-            ),
-            data={"license_number": test_license_number}
+            reverse("taxi:driver-update", kwargs={"pk": self.user.id}),
+            data={"license_number": test_license_number},
         )
         self.assertEqual(response.status_code, 200)
 
@@ -67,7 +61,7 @@ class DriverViewsTest(TestCase):
             license_number="NOT12345",
             first_name="Not Admin",
             last_name="User",
-            password="1qazcde3"
+            password="1qazcde3",
         )
         response = self.client.post(
             reverse("taxi:driver-delete", kwargs={"pk": driver.id})
